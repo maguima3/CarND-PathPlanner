d28c3ac03c4e751d9f229cfd4ad879a167e3acb7
Maria Guinea <m.guinea10@gmail.com>
2018-03-11 00:51:22 +0100

    drives smooth and safe

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 
 #define MPH_TO_MS 0.44 //conversion factor
 
-const double max_velocity = 49.8 * MPH_TO_MS; // miles per hour
+const double max_velocity = 49.5 * MPH_TO_MS; // miles per hour
 const double horizon = 50;
 const double points_per_second = 50; // The car visits one point every .02 seconds
 const double lane_width = 4.0; //in meters
@@ -193,18 +193,17 @@ class Vehicle {
   double vy;
   double yaw;
 
-
-  Vehicle(double x, double y, double s, double d) :
-    x(x), y(y), s(s), d(d) {}
+  Vehicle(double x=0, double y=0, double s=0, double d=0) :
+    x(x), y(y), s(s), d(d), id(-1) {}
 
   void setVelocity(double vx, double vy) {
     this->vx = vx;
     this->vy = vy;
   }
-  void setId(double id) {
+    void setId(double id) {
     this->id = id;
   }
-  double getSpeed() {
+double getSpeed() {
     return sqrt(vx*vx + vy*vy);
   }
 };
@@ -327,10 +326,12 @@ int main() {
           	const double min_gap = 50.0;
           	// Range where other vehicles are detected
           	const double sensor_range = 100.0; //meters
-          	const double safety_distance = 20.0; // There's risk of collision if other cars are closer [meters]
+          	const double safety_distance = 30.0; // There's risk of collision if other cars are closer [meters]
+          	const double safety_distance_behind = 5.0;
           	const double brake_distance = 30.0; // Break if there is any car closer
 
           	double closest_car_distance = 20000000; // Indicates the distance of the closest car in our lane. Controls acceleration
+          	Vehicle car_infront;
 
           	// Check other cars in the road
           	for (int i=0; i<sensor_fusion.size(); ++i) {
@@ -386,14 +387,16 @@ int main() {
           	  // If it is too close, we need to change lane!
 
           	  if (other.d>lane*lane_width && other.d<(lane+1)*lane_width) {
-          	    double cars_lane_distance = abs(other.s-ego.s);
-          	    bool is_too_close = (is_ahead) && (cars_lane_distance < min_gap);
+          	    double car_infront_distance = abs(other.s-ego.s);
+          	    bool is_too_close = (is_ahead) && (car_infront_distance < min_gap);
 
           	    if (is_too_close) {
           	      change_lane = true;
 
-          	      if (cars_lane_distance < closest_car_distance){
-                  closest_car_distance = cars_lane_distance;
+          	      car_infront = other;
+
+          	      if (car_infront_distance < closest_car_distance){
+                  closest_car_distance = car_infront_distance;
                 }
           	      //printf("Vehicle too close on lane %f. It is %f meters ahead\n", lane, (other.s - ego.s));
           	    }
@@ -436,18 +439,18 @@ int main() {
               if (lane != 1) {
                 // Check vehicles in the center lane and look for a gap
                 for (auto other = cars_center_ahead.begin(); other!=cars_center_ahead.end(); ++other) {
-                  //double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
-                  //double distance = (other_future_s-car_s);
-                  double distance = abs(other->s - car_s);
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("CA - Gap in meters: %f\n", distance);
                   if (distance<min_distance_ahead) {
                     min_distance_ahead = distance;
                   }
                 }
                 for (auto other = cars_center_behind.begin(); other!=cars_center_behind.end(); ++other) {
-                  //double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
-                  //double distance = (other_future_s-car_s);
-                  double distance = abs(other->s - car_s);
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("CB - Gap in meters: %f\n", distance);
                   if (distance<min_distance_behind) {
                     min_distance_behind = distance;
@@ -455,11 +458,10 @@ int main() {
                 }
 
                 if (min_distance_ahead>safety_distance) {
-                  //lane = 1;
                   car_ahead = false;
                   printf("No cars ahead! Closest car is at %f meters\n", min_distance_ahead);
                 }
-                if (abs(min_distance_behind)>safety_distance) {
+                if (abs(min_distance_behind)>safety_distance_behind) {
                   printf("No cars behind! Closest car is at %f meters\n", min_distance_behind);
                   car_behind = false;
                 }
@@ -473,19 +475,26 @@ int main() {
               else {
                 bool left_change = false;
                 bool right_change = false;
+                double distance_ahead_left;
+                double distance_ahead_right;
+                Vehicle closest_ahead_left;
+                Vehicle closest_ahead_right;
 
                 // First check if we can move left
-                for (auto other = cars_left_ahead.begin(); other!=cars_left_ahead.end(); ++other) {
-                  //double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
-                  //double distance = (other_future_s-car_s);
-                  double distance = abs(other->s - car_s);
+                for (vector<Vehicle>::iterator other = cars_left_ahead.begin(); other!=cars_left_ahead.end(); ++other) {
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("LA - Gap in meters: %f\n", distance);
                   if (distance<min_distance_ahead) {
                     min_distance_ahead = distance;
+                    closest_ahead_left = *other;
                   }
                 }
                 for (auto other = cars_left_behind.begin(); other!=cars_left_behind.end(); ++other) {
-                  double distance = abs(other->s - car_s);
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("LB - Gap in meters: %f\n", distance);
                   if (distance<min_distance_behind) {
                     min_distance_behind = distance;
@@ -494,9 +503,10 @@ int main() {
 
                 if (min_distance_ahead>safety_distance) {
                   car_ahead = false;
+                  distance_ahead_left = min_distance_ahead;
                   printf("No cars ahead! Closest car is at %f meters\n", min_distance_ahead);
                 }
-                if (min_distance_behind>safety_distance) {
+                if (min_distance_behind>safety_distance_behind) {
                   printf("No cars behind! Closest car is at %f meters\n", min_distance_behind);
                   car_behind = false;
                 }
@@ -511,17 +521,21 @@ int main() {
                 car_ahead = true;
                 car_behind = true;
 
+
                 for (auto other = cars_right_ahead.begin(); other!=cars_right_ahead.end(); ++other) {
-                  //double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
-                  //double distance = (other_future_s-car_s);
-                  double distance = abs(other->s - car_s);
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("RA - Gap in meters: %f\n", distance);
                   if (distance<min_distance_ahead) {
                     min_distance_ahead = distance;
+                    closest_ahead_right = *other;
                   }
                 }
                 for (auto other = cars_right_behind.begin(); other!=cars_right_behind.end(); ++other) {
-                  double distance = abs(other->s - car_s);
+                  double other_future_s = other->s + other->getSpeed() * prev_size / points_per_second;
+                  double distance = abs(other_future_s-car_s);
+                  //double distance = abs(other->s - car_s);
                   printf("RB - Gap in meters: %f\n", distance);
                   if (distance<min_distance_behind) {
                     min_distance_behind = distance;
@@ -530,9 +544,10 @@ int main() {
 
                 if (min_distance_ahead>safety_distance) {
                   car_ahead = false;
+                  distance_ahead_right = min_distance_ahead;
                   printf("No cars ahead! Closest car is at %f meters\n", min_distance_ahead);
                 }
-                if (min_distance_behind>safety_distance) {
+                if (min_distance_behind>safety_distance_behind) {
                   printf("No cars behind! Closest car is at %f meters\n", min_distance_behind);
                   car_behind = false;
                 }
@@ -550,17 +565,52 @@ int main() {
                 }
                 else if (left_change && right_change) {
                   // Move to lane with less traffic
-                  size_t traffic_left = cars_left_ahead.size();
-                  size_t traffic_right = cars_right_ahead.size();
+                  //size_t traffic_left = cars_left_ahead.size();
+                  //size_t traffic_right = cars_right_ahead.size();
 
-                  lane = traffic_left<traffic_right ? 0 : 2;
+                  //lane = traffic_left<traffic_right ? 0 : 2;
+
+                  // Move to the lane with furtherest car ahead
+                  //lane = distance_ahead_left>distance_ahead_right ? 0 : 2;
+
+                  // Move to other line if the vehicles are faster than the car in front of us
+                  bool car_left_faster = closest_ahead_left.getSpeed()>car_infront.getSpeed() ? true : false;
+                  bool car_right_faster = closest_ahead_right.getSpeed()>car_infront.getSpeed() ? true : false;
+                  if (closest_ahead_left.id < 0 || closest_ahead_right.id < 0) {
+                    // If there are no cars on the left or no cars right
+                    // Move to lane with less traffic
+                    lane = cars_left_ahead.size()<cars_right_ahead.size() ? 0 : 2;
+                  }
+                  else if (car_left_faster && !car_right_faster) {
+                    lane = 0;
+                  }
+                  else if (!car_left_faster && car_right_faster) {
+                    lane = 2;
+                  }
+                  else if (car_left_faster && car_right_faster) {
+                    // Move to the lane with the fastest car ahead
+                     lane = closest_ahead_left.getSpeed()>closest_ahead_right.getSpeed() ? 0 : 2;
+
+                  }
+                  else if (!car_left_faster && !car_right_faster){
+                    // Cars on the left and right are slower. Better keep in the middle
+                    lane = 0;
+                  }
+
                 }
               }
           	}
 
             // Reduce target velocity if we are too close to other cars
             if (closest_car_distance < brake_distance) {
-              ref_velocity -= .6; // m/s
+
+              if (ref_velocity > car_infront.getSpeed()) {
+                ref_velocity -= .2; // m/s
+
+              } else {
+                ref_velocity += .2; // m/s
+              }
+
             } else if (ref_velocity < max_velocity){
               ref_velocity += .6;
             }
